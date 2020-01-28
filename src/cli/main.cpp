7f6085f832daa22ebaef8c5fe7c1863d6505ead3
Mamedaliev Kirill <danteg41@gmail.com>
2020-01-28 18:30:33 +0500

    add zbotcli: zconfig class & print help

---
+++ src/cli/main.cpp
@@ -0,0 +1,25 @@
+#include <getopt.h>
+#include <zconfig.h>
+
+int main(int argc, char *argv[]) {
+  int opt;
+  ZConfig config;
+
+  while ((opt = getopt(argc,argv,"hf:")) != -1) {
+    switch (opt) {
+      case 'f':
+      {
+          config.configFile = optarg;
+	       break;
+      }
+      default:
+	       fprintf(stderr, "Usage: %s [-f configfile] -[c|C] chat -m message\n\
+            -f\t\tconfiguration file path\n\
+            -c\t\tchat recipient name\n\
+            -C\t\tchat recipient ID\n\
+            -m\t\tmessage (max length 256 characters)\n", argv[0]);
+	       exit(EXIT_FAILURE);
+    }
+  }
+  config.load();
+}
