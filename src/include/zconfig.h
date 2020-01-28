db7776f5e19066fc8a3cc69e8f4f2471cc725d45
Mamedaliev Kirill <danteg41@gmail.com>
2020-01-28 19:04:37 +0500

    improve formatting

--- src/include/zconfig.h
+++ src/include/zconfig.h
@@ -1,29 +1,31 @@
 #include <map>
 #include <string>
 
-class ZConfig {
-    public:
-        const char* configFile;
-        ZConfig(const char* c) { configFile = c; };
-        ZConfig() { configFile = "./test.ini"; };
+class ZConfig
+{
+public:
+    const char *configFile;
+    ZConfig(const char *c) { configFile = c; };
+    ZConfig() { configFile = "./test.ini"; };
 
-        void load();
-        void getParam(const char* name, int&);
-        void getParam(const char* name, std::string&);
-    private:
-       std::map <std::string, std::string>::iterator paramsIter_;
-       std::map <std::string, std::string> params_ = {
-           {"storage", "/var/spool/zbot"},
-           {"log", "/var/log/zbot.log"},
-           {"wait", "10"}
-       };
+    void load();
+    void getParam(const char *name, int &);
+    void getParam(const char *name, std::string &);
+
+private:
+    std::map<std::string, std::string>::iterator paramsIter_;
+    std::map<std::string, std::string> params_ = {
+        {"storage", "/var/spool/zbot"},
+        {"log", "/var/log/zbot.log"},
+        {"wait", "10"}};
 };
 
-class ZConfigException {
-    private:
-        std::string m_error;
+class ZConfigException
+{
+private:
+    std::string m_error;
 
-    public:
-        ZConfigException(std::string error) : m_error(error) {}
-        const char* getError() { return m_error.c_str(); }
+public:
+    ZConfigException(std::string error) : m_error(error) {}
+    const char *getError() { return m_error.c_str(); }
 };
